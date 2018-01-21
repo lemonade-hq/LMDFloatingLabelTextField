dc1ecb117b1044b778f94b21f1aac61efc4714fe
Tal Cohen <tal.cohen@lemonade.com>
2018-01-21 12:36:19 +0200

    Support prefilled text

--- Example/LMDTextField/ViewController.swift
+++ Example/LMDTextField/ViewController.swift
@@ -10,29 +10,30 @@ import UIKit
 import LMDFloatingLabelTextField
 
 class ViewController: UIViewController {
-
-    @IBOutlet weak var emailTextField: LMDFloatingLabelTextField!
-    @IBOutlet weak var usernameTextField: LMDFloatingLabelTextField!
-    @IBOutlet weak var passwordTextField: LMDFloatingLabelTextField!
+    
+    weak var textField: LMDFloatingLabelTextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.emailTextField.keyboardType = .emailAddress
-        [self.usernameTextField, self.emailTextField, self.passwordTextField]
-            .forEach { $0?.returnKeyType = .next }
-        
         let tf = LMDFloatingLabelTextField(frame: CGRect(x: 20, y: 400, width: 200, height: 48))
         tf.placeholderText = "placeholder"
+        self.textField = tf
+        tf.text = "Prefilled field"
         self.view.addSubview(tf)
     }
+    @IBAction func errorSwitchValueChanged(_ errorSwitch: UISwitch) {
+        self.textField.error = errorSwitch.isOn
+    }
     
-    @IBAction func error(_ sender: Any) {
-        if let text = usernameTextField?.text, text.count < 6 {
-            self.usernameTextField.error = true
-        }
-        
-        if emailTextField.text?.contains("@") == false {
-            self.emailTextField.error = true
-        }
+    @IBAction func enableButtonTapped(_ sender: Any) {
+        self.textField.disabled = false
+    }
+    
+    @IBAction func disableButtonTapped(_ sender: Any) {
+        self.textField.disabled = true
+    }
+    
+    @IBAction func endEditingButtonTapped(_ sender: Any) {
+        self.textField.endEditing(true)
     }
 }
--- LMDFloatingLabelTextField/LMDFloatingLabelTextField.swift
+++ LMDFloatingLabelTextField/LMDFloatingLabelTextField.swift
@@ -11,14 +11,13 @@ import UIKit
 open class LMDFloatingLabelTextField: UITextField {
     
     public enum State {
-        case disabled
         case editing
         case notEditing
     }
     
     fileprivate var lmd_state : State = .notEditing {
         didSet {
-            self.animatePlaceholderIfNeeded(with: lmd_state, previousState: oldValue)
+            self.animatePlaceholderIfNeeded(with: lmd_state)
         }
     }
     
@@ -26,6 +25,7 @@ open class LMDFloatingLabelTextField: UITextField {
     fileprivate let textRectYInset : CGFloat = 7
     fileprivate var editingConstraints = [NSLayoutConstraint]()
     fileprivate var notEditingConstraints : [NSLayoutConstraint]!
+    fileprivate var activeConstraints : [NSLayoutConstraint]!
     
     //MARK: - PUBLIC VARIABLES
     public var placeholderFont = UIFont.systemFont(ofSize: 14) {
@@ -34,9 +34,16 @@ open class LMDFloatingLabelTextField: UITextField {
         }
     }
     
+    open override var text: String? {
+        didSet {
+            self.animatePlaceholderIfNeeded(with: self.lmd_state)
+        }
+    }
+    
     @IBInspectable public var placeholderText: String? {
         didSet {
-            self.setNeedsLayout()
+            self.lmd_placeholder.text = placeholderText
+            self.animatePlaceholderIfNeeded(with: self.lmd_state)
         }
     }
     
@@ -94,6 +101,12 @@ open class LMDFloatingLabelTextField: UITextField {
         }
     }
     
+    public var disabled = false {
+        didSet {
+            self.updateState(.notEditing)
+        }
+    }
+    
     public var error = false {
         didSet {
             self.setNeedsLayout()
@@ -136,12 +149,17 @@ open class LMDFloatingLabelTextField: UITextField {
             NSLayoutConstraint(item: self.lmd_placeholder, attribute: .left, relatedBy: .equal, toItem: self, attribute: .left, multiplier: 1, constant: self.leftPadding),
             NSLayoutConstraint(item: self.lmd_placeholder, attribute: .centerY, relatedBy: .equal, toItem: self, attribute: .centerY, multiplier: 1, constant: 0)
         ]
-        
-        self.animatePlaceholderIfNeeded(with: .notEditing, previousState: nil)
+        NSLayoutConstraint.activate(self.notEditingConstraints)
+        self.activeConstraints = self.notEditingConstraints
+        self.setNeedsLayout()
     }
     
     fileprivate func calculateEditingConstraints() {
-        let originalWidth = self.lmd_placeholder.frame.width
+        let attributedStringPlaceholder = NSAttributedString(string: (self.placeholderText ?? "").uppercased(), attributes: [
+            NSAttributedStringKey.font : self.placeholderFont
+            ])
+        let originalWidth = attributedStringPlaceholder.boundingRect(with: CGSize(width: .greatestFiniteMagnitude, height: self.frame.height), options: [], context: nil).width
+        
         let xOffset = (originalWidth - (originalWidth * placeholderSizeFactor)) / 2
         
         self.editingConstraints = [
@@ -156,41 +174,60 @@ open class LMDFloatingLabelTextField: UITextField {
         self.lmd_placeholder.font = self.placeholderFont
         self.lmd_placeholder.textColor = self.placeholderTextColor
         self.lmd_placeholder.text = self.placeholderText?.uppercased()
-        self.textColor = self.lmd_state == .disabled ? self.disabledTextColor : self.textFieldTextColor
-        self.backgroundColor = self.lmd_state == .disabled ? self.disabledBackgroundColor : self.enabledBackgroundColor
+        self.textColor = self.disabled ? self.disabledTextColor : self.textFieldTextColor
+        self.backgroundColor = self.disabled ? self.disabledBackgroundColor : self.enabledBackgroundColor
         self.layer.borderColor = self.error ? self.errorBorderColor?.cgColor : self.lmd_state == .editing ?
             self.borderColor?.cgColor :
             UIColor(white: 236/255, alpha: 1).cgColor
-        self.isEnabled = self.lmd_state != .disabled
+        self.isEnabled = !self.disabled
     }
     
-    fileprivate func animatePlaceholderIfNeeded(with state: State, previousState: State?) {
-        guard state != previousState else { return }
+//    fileprivate func animatePlaceholderIfNeeded(with state: State, previousState: State?) {
+    fileprivate func animatePlaceholderIfNeeded(with state: State) {
+        
         switch state {
-        case .disabled:
-            break
-            
         case .editing:
-            if previousState == .notEditing && (self.text ?? "").isEmpty == true {
-                self.animatePlaceholderToActivePosition()
-            }
-            
+            self.animatePlaceholderToActivePosition()
         case .notEditing:
-            if (self.text ?? "").isEmpty == true {
-                if previousState == .editing {
-                    self.animatePlaceholderToInactivePosition()
-                } else if previousState == nil {
-                    self.animatePlaceholderToInactivePosition(animated: false)
-                }
+            if (self.text ?? "").isEmpty {
+                self.animatePlaceholderToInactivePosition()
+            } else {
+                self.animatePlaceholderToActivePosition()
             }
         }
         self.setNeedsLayout()
+    
+//        guard state != previousState else { return }
+//        switch state {
+//        case .disabled:
+//            break
+//
+//        case .editing:
+//            if previousState == .notEditing && (self.text ?? "").isEmpty == true {
+//                self.animatePlaceholderToActivePosition()
+//            } else if previousState == nil {
+//                self.animatePlaceholderToActivePosition(animated: false)
+//            }
+//
+//        case .notEditing:
+//            if (self.text ?? "").isEmpty == true {
+//                if previousState == .editing {
+//                    self.animatePlaceholderToInactivePosition()
+//                } else if previousState == nil {
+//                    self.animatePlaceholderToInactivePosition(animated: false)
+//                }
+//            }
+//        }
+//        self.setNeedsLayout()
     }
     
     fileprivate func animatePlaceholderToActivePosition(animated: Bool = true) {
-        NSLayoutConstraint.deactivate(self.notEditingConstraints)
         self.calculateEditingConstraints()
+        self.layoutIfNeeded()
+        NSLayoutConstraint.deactivate(self.activeConstraints)
         NSLayoutConstraint.activate(self.editingConstraints)
+        self.activeConstraints = self.editingConstraints
+        
         let animationBlock = {
             self.layoutIfNeeded()
             self.lmd_placeholder.transform = CGAffineTransform(scaleX: self.placeholderSizeFactor, y: self.placeholderSizeFactor)
@@ -205,8 +242,10 @@ open class LMDFloatingLabelTextField: UITextField {
     }
     
     fileprivate func animatePlaceholderToInactivePosition(animated: Bool = true) {
-        NSLayoutConstraint.deactivate(self.editingConstraints)
+        self.layoutIfNeeded()
+        NSLayoutConstraint.deactivate(self.activeConstraints)
         NSLayoutConstraint.activate(self.notEditingConstraints)
+        self.activeConstraints = self.notEditingConstraints
         let animationBlock = {
             self.layoutIfNeeded()
             self.lmd_placeholder.transform = .identity
